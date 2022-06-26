8dd271a34d3b804c0a2e383d1eb5307c7c99560a
Lucas <lucas.santos@tallos.com.br>
2022-06-26 18:23:22 -0300

    feat: Adicionando validacoes de infomacoes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
@@ -10,6 +11,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(4000);
 }
 console.log("subiu")
--- src/usuarios/model/user.model.ts
+++ src/usuarios/model/user.model.ts
@@ -1,21 +1,28 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, Matches, IsNotEmpty } from 'class-validator';
 import { Document } from 'mongoose';
 
 export type UserDocument = User & Document;
 @Schema()
 export class User {
-
     @Prop({ required: true })
     @ApiProperty()
+    @IsNotEmpty({
+        message: 'Digite um nome de Usuário'
+    })
     name: string;
 
-    @Prop({ required: true, unique:true })
+    @Prop({ required: true, unique: true })
     @ApiProperty()
+    @IsEmail({}, { message: 'Digite um endereço de email válido.' })
     email: string;
 
     @Prop({ required: true })
     @ApiProperty()
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+        message: 'Senha Fraca.',
+    })
     password: string;
 
 }
