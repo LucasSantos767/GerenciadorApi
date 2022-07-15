1c66ccbe136548f641900dc166d1abde5bf541de
Lucas <lucas.santos@tallos.com.br>
2022-07-15 14:58:20 -0300

    feat: Adicionado cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('Cats example')
     .setDescription('The cats API description')
