144d3a31e9ca371af9e54a3bf03cef161163d371
Lucas <lucas.santos@tallos.com.br>
2022-07-26 14:34:59 -0300

    feat: Adicionando eventos do socket

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,18 @@
-import { JwtService } from '@nestjs/jwt';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose'
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { AuthModule } from './auth/auth.module';
+import { SocketModule } from './socket/socket.module';
+import { SocketGateway } from './socket/socket.gateway';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://root:root@db:27017/manager?authSource=admin'),
     UsuariosModule,
     AuthModule,
+    SocketModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [SocketGateway],
 })
 export class AppModule { }
--- src/usuarios/usuarios.module.ts
+++ src/usuarios/usuarios.module.ts
@@ -5,12 +5,13 @@ import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { UsuariosController } from './controllers/usuarios.controller';
 import { UsuariosService } from './service/usuarios.service';
 import { User, UserSchema } from './model/user.model';
+import { SocketGateway } from 'src/socket/socket.gateway';
 
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
   controllers: [UsuariosController],
-  providers: [UsuariosService,{
+  providers: [SocketGateway, UsuariosService,{
     provide: APP_GUARD,
     useClass: JwtAuthGuard
   }],
